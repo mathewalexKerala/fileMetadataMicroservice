0a40edceceaf381ad2d7edebe0076e51ac198c65
mathewalexKerala <mathewalexmasai@gmail.com>
2024-08-19 10:14:06 +0530

    npm uploaded

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
 var express = require('express');
 var cors = require('cors');
-require('dotenv').config()
-
 var app = express();
+require('dotenv').config()
+const multer = require('multer');
+const bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
+const upload = multer({ dest: 'uploads/' });
 app.use(cors());
 app.use('/public', express.static(process.cwd() + '/public'));
 
@@ -11,7 +15,17 @@ app.get('/', function (req, res) {
   res.sendFile(process.cwd() + '/views/index.html');
 });
 
+app.post('/api/fileanalyse', upload.single('upfile'), (req, res) => {
+  if (!req.file) {
+    return res.status(400).json({ error: 'No file uploaded' });
+  }
 
+  res.json({
+    name: req.file.originalname,
+    type: req.file.mimetype,
+    size: req.file.size
+  });
+});
 
 
 const port = process.env.PORT || 3000;
